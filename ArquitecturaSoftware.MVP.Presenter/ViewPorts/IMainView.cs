b67655dfcb329f1230895b1074644db10ab19151
Juan Sebastian Ramirez <juansebastian@innovati.com.co>
2021-09-06 16:24:57 -0500

    Create MVP structure

---
+++ ArquitecturaSoftware.MVP.Presenter/ViewPorts/IMainView.cs
@@ -0,0 +1,13 @@
+﻿namespace ArquitecturaSoftware.MVP.Presenter.ViewPorts
+{
+    public interface IMainView
+    {
+        void SetImage(string urlImage);
+
+        void SetTitle(string title);
+
+        void HideLogInForm();
+
+        void EnableGetImageButton();
+    }
+}
